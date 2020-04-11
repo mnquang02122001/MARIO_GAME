3116ad6ed150c57be121046341aa3326f5774286
Mai Nhat Quang <mnquang123@gmail.com>
2020-04-11 23:01:48 +0700

    Remove binary directory

--- main.cpp
+++ main.cpp
@@ -47,7 +47,7 @@ bool InitData() {
 
 }
 bool LoadBackground() {
-    bool ret = g_background.LoadImg("img//background.png", g_screen);
+    bool ret = g_background.LoadImg("img//BACKGROUND.jpg", g_screen);
     if (!ret) {
         return false;
     }
