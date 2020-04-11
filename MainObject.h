9a70bbc3f29e30b43ed650cb2a350d28f5051c01
Mai Nhat Quang <mnquang123@gmail.com>
2020-04-11 22:32:21 +0700

    Remove binary directory

---
+++ MainObject.h
@@ -0,0 +1,29 @@
+#ifndef MAIN_OBJECT_H
+#define MAIN_OBJECT_H
+
+#include"CommonFunc.h"
+#include"BaseObject.h"
+
+class MainObject : public BaseObject {
+public: 
+	MainObject();
+	~MainObject();
+	bool LoadImg(std::string path, SDL_Renderer* screen);
+	void Show(SDL_Renderer* des);
+	void HandleInputAction(SDL_Event events, SDL_Renderer* screeen);
+
+private:
+	float x_val_;
+	float y_val_;
+
+	float x_pos_;
+	float y_pos_;
+
+	int width_frame_;
+	int height_frame_;
+
+	SDL_Rect frame_clip_[8];
+};
+
+#endif // !MAIN_OBJECT_H
+
